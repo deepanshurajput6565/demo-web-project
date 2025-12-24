15e8644ac8221672b592b34a8c423df07ee2f91d
deepanshurajput6565 <deepanshurajput6565@gmail.com>
2025-12-24 11:47:07 +0530

    Add files via upload

---
+++ logic building .js
@@ -0,0 +1,53 @@
+// // average of three number entered by user
+
+// let mark1 = parseFloat(prompt("enter your first number : "));
+// let mark2 = parseFloat(prompt("enter your second number : "));
+// let mark3 = parseFloat(prompt("enter your third number : "));
+
+// let average = (mark1 + mark2 + mark3) / 3;
+// console.log(average);
+
+// program for factorial of any number
+
+let num = parseInt(prompt("Enter a number: "));
+let fact = 1;
+
+for (let i = 1; i <= num; i++) {
+  fact *= i;
+}
+console.log(fact);
+
+// even or odd number
+
+// let num = parseInt(prompt("enter"));
+// if (num % 2 === 0) {
+//     console.log("even");
+// }
+// else {
+//     console.log("odd");
+// }
+
+// switch staement
+
+// let day = parseInt(prompt("enter days number"));
+// let dayname;
+
+// switch (day) {
+//     case 0:
+//         dayname = "sunday";
+//         break;
+
+//     case 1:
+//         dayname = "monday";
+//         break;
+
+//     case 2:
+//         dayname = "tuesday";
+//         break;
+//     default:
+//         dayname = "invalid number";
+//         break;
+
+// }
+
+// console.log(dayname);
