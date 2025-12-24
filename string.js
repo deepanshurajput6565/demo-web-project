c2843e8d12a465e0c41ed512c4f8100bcbc3bad8
deepanshurajput6565 <deepanshurajput6565@gmail.com>
2025-12-24 11:47:30 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,19 @@
+// let s1 = "deepak kumar singh";
+// let s2 ="lucky singh";
+// // String.length is used to sring length
+// let len = s1.length;
+// console .log ("string length :",+len);
+
+// // concentented is used to add the string 
+// let res = s1+s2;
+// console.log("concentented string :",+res);
+
+// // \ is used to break the string
+// let s3= "i study in \n college";
+// console .log(s3);
+
+// // [n] is use  to acces the character of sting
+// let s4=s1[13];
+// console.log(s4);
+
+
