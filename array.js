a064c6c0269143b1f19bf1e0dbd9f50c2642c00e
deepanshurajput6565 <deepanshurajput6565@gmail.com>
2025-12-24 11:44:40 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,21 @@
+// let lang =["HTml"," CSS", "js", "nodejs","reactjs"];
+// // acceseeing the element of arary
+// console.log(lang[2]);
+
+// // accesssing the last item 
+// let lst = lang[lang.length-1];
+// console.log("last item :",lst);
+
+// // to obtain the length of the array
+// let len =lang.length;
+// console.log("array length:",+len);
+
+// // you can also access the element of Array by using for loop and for eeach loop
+// for (let i=0; i<lang.length;i++){
+//     console.log(lang[i])
+// }
+// // to convert Array into String
+// console .log(lang.toString());
+
+// // to check the types of array 
+// console.log(typeof lang);
