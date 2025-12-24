ad8e05b5380fd5430809052cdd6280f17f1f0e81
deepanshurajput6565 <deepanshurajput6565@gmail.com>
2025-12-24 11:46:09 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,7 @@
+// alert(hi) we use this option when we use browser not an on node.
+let age = "18";
+
+//  let ValueinString=string(age)
+// console.log(typeof age)
+let valueInNumber = Number(age);
+console.log(typeof (valueInNumber));
