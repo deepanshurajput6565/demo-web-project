aa30afe1c2371dd20d03c30bdd6a13c6a49b5da8
deepanshurajput6565 <deepanshurajput6565@gmail.com>
2025-12-24 11:45:43 +0530

    Add files via upload

---
+++ conditionalstatement.js
@@ -0,0 +1,92 @@
+// to implement  any condition
+// if statement,if else ,if else if else,ternary    
+
+// 1.if else  statement 
+// let x=67;
+// if( x%2===0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// };
+
+// 2.else if statement
+// const x=56;
+// if (x>0){
+//     console.log("positive");
+
+// }
+// else if (x<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("zero");
+// }
+
+// 3. switch Statement
+// const mark = 94;
+// let branch;
+// switch (true) {
+//     case mark >= 90:
+//         branch = "cse";
+//         break;
+
+//     case mark >= 80  :
+//         branch = "me";
+//         break;
+
+
+//     case mark >= 70:
+//         branch = "ece";
+//         break;
+// }
+// console.log("branch of student is=");
+// console.log(branch);
+
+// 4. ternary  statement
+// let age = 24;
+// const result =
+//     (age => 18) ? "you are eligible for voitng!!" : "you are not eligible for voting!!";
+// console.log(result);
+
+// 5. nested if else
+
+// let weather = "sunny";
+// let temp = 15;
+// if (weather === "sunny") {
+//     if (temp > 30) {
+//         console.log("it is hot today !");
+//     } else if (temp > 20) {
+//         console.log("it is a warm day");
+//     }
+//     else {
+//         console.log("it is a bit cool day");
+//     }
+// }
+// else if (weather === "rainy") {
+//     console.log("don't forget your umbrella!");
+// }
+// else {
+//     console.log("check the weather forcast!");
+// }
+
+
+// const useLog=true
+// const debit=false
+// if(useLog && debit){
+//     console.log("user is allow");
+// } 
+// else {
+//     console.log("user is not allowed");
+
+// }   
+
+
+// switch(key) {
+//     case Value:
+
+//     break;
+
+//     default:
+//         break;
+// }
